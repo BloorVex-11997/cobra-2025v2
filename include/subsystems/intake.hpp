82bdb9a6d6638f381f1f67e5058549c606ed2427
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-03-05 00:44:23 -0500

    Add subsystems

---
+++ include/subsystems/intake.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "uvlib/subsystem.hpp"
+class Intake : public uvl::Subsystem {
+public:
+  Intake();
+
+  void set_voltage(int intake_voltage, int elevator_voltage);
+
+  void enable();
+
+  void disable();
+
+private:
+  pros::Motor m_intake_motor;
+  pros::Motor m_elevator_motor;
+};
